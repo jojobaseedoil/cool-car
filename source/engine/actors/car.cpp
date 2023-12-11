2a57e8c17f20ee1f5988f20e74ea90a3d3f08301
jojobaseedoil <caiorondow@gmail.com>
2023-12-11 14:33:22 -0300

    +car gearbox, +tp1 finished

--- source/engine/actors/car.cpp
+++ source/engine/actors/car.cpp
@@ -15,11 +15,18 @@ Car::Car(InterfaceScene *scene):
     Actor(scene),
     mCarWidth(CAR_WIDTH),
     mCarHeight(CAR_HEIGHT),
-    mCameraDelay(0.0f)
+    mSpeed(NEUTRAL_GEAR_SPEED),
+    mCurrentGear(0),
+    mCanProcessInput(false)
 {   
     new DrawComponent(this, "../models/cool-car.obj");
     new RigidBodyComponent(this, 1, 4);
 
+    mGearBox[0] = NEUTRAL_GEAR_SPEED;
+    mGearBox[1] = FIRST_GEAR_SPEED;
+    mGearBox[2] = SECOND_GEAR_SPEED;
+    mGearBox[3] = THIRD_GEAR_SPEED;
+    mGearBox[4] = REVERSE_GEAR;
 
     for(int i=0; i<100; i++){
         Wheel *w = new Wheel(scene, false);
@@ -35,59 +42,87 @@ void Car::OnUpdate(float DeltaTime){
     RotationMatrix = glm::rotate(RotationMatrix, glm::radians(-180.0f), glm::vec3(0,0,1));
     SetModel(RotationMatrix);
 
-    std::cout << GetPosition().x << " ";
-    std::cout << GetPosition().y << " ";
-    std::cout << GetPosition().z << "\n\n";
+    // std::cout << GetPosition().x << " ";
+    // std::cout << GetPosition().y << " ";
+    // std::cout << GetPosition().z << "\n\n";
+
+    /* simple input handle system */
+    static float timer = 0;
+    timer += DeltaTime;
+    if(timer >= 1.0f){ /* wait 1 second */
+        mCanProcessInput = true;
+        timer = 0.0f;
+    }
 }
 
 void Car::OnProcessInput(GLFWwindow *window){
 
+    if( glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) ){ /* press shift for reverse gear */
+
+        mSpeed = mGearBox[4];
+
+    } else if( glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && mCanProcessInput){ /* change gears */
+        mCurrentGear = (mCurrentGear+1)%4;
+        mSpeed = mGearBox[mCurrentGear];
+        mCanProcessInput = false;
+    } else { /* move with the current gear */
+        mSpeed = mGearBox[mCurrentGear];
+    }
+
+    /* move up */
     if( glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS ){
-        Move(glm::vec3(0,1,0), CAR_FORWARD_SPEED);
+        Move(glm::vec3(0,1,0)*mSpeed);
         SetRotation(0);
     } 
 
+    /* move to right */
     if( glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS ){
-        Move(glm::vec3(1,0,0), CAR_FORWARD_SPEED);
+        Move(glm::vec3(1,0,0)*mSpeed);
         SetRotation(-90);
     }
 
+    /* move to down */
     if( glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS ){
-        Move(glm::vec3(0,-1,0), CAR_FORWARD_SPEED);
+        Move(glm::vec3(0,-1,0)*mSpeed);
         SetRotation(180);
     } 
 
+    /* move to left */
     if( glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS ){
-        Move(glm::vec3(-1,0,0), CAR_FORWARD_SPEED);
+        Move(glm::vec3(-1,0,0)*mSpeed);
         SetRotation(90);
     }
-
+    
+    /* move up right */
     if( glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS ){
-        Move(glm::vec3(1,1,0), CAR_FORWARD_SPEED/4);
+        Move(glm::vec3(1,1,0)*mSpeed/4.0f);
         SetRotation(-45);
     }
 
+    /* move up left */
     if( glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS ){
-        Move(glm::vec3(-1,1,0), CAR_FORWARD_SPEED/4);
+        Move(glm::vec3(-1,1,0)*mSpeed/4.0f);
         SetRotation(45);
     }  
 
+    /* move down right */
     if( glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS ){
-        Move(glm::vec3(1,-1,0), CAR_FORWARD_SPEED/4);
+        Move(glm::vec3(1,-1,0)*mSpeed/4.0f);
         SetRotation(-135);
     }  
 
+    /* move to down left */
     if( glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS ){
-        Move(glm::vec3(-1,-1,0), CAR_FORWARD_SPEED/4);
+        Move(glm::vec3(-1,-1,0)*mSpeed/4.0f);
         SetRotation(135);
-    }  
+    } 
 }
 
-void Car::Move(const glm::vec3 &dir, const float speed){
+void Car::Move(const glm::vec3 &force){
     
-    this->GetComponent<RigidBodyComponent>()->ApplyForce(dir*speed);
+    this->GetComponent<RigidBodyComponent>()->ApplyForce(force);
     
     glm::vec3 CarPos = GetPosition();
     Camera *camera = mScene->GetCamera();
-    camera->GetComponent<RigidBodyComponent>()->ApplyForce(dir*(speed/4));
+    camera->GetComponent<RigidBodyComponent>()->ApplyForce(force/4.0f);
 }
--- source/engine/actors/car.h
+++ source/engine/actors/car.h
@@ -2,6 +2,12 @@
 
 #include "actor.h"
 
+#define NEUTRAL_GEAR_SPEED 0.0f
+#define FIRST_GEAR_SPEED 100.0f
+#define SECOND_GEAR_SPEED 150.0f
+#define THIRD_GEAR_SPEED 175.0f
+#define REVERSE_GEAR -50.0f
+
 class Wheel;
 class Chassis;
 class RigidBodyComponent;
@@ -13,8 +19,11 @@ public:
     void OnUpdate(float DeltaTime) override;
     void OnProcessInput(GLFWwindow *window) override;
 
-    void Move(const glm::vec3 &dir, const float speed);
+    void Move(const glm::vec3 &force);
 private:
     float mCarWidth, mCarHeight;
-    float mCameraDelay;
+    float mGearBox[5]; // marcha do carro
+    float mSpeed;
+    int mCurrentGear;
+    bool mCanProcessInput;
 };
